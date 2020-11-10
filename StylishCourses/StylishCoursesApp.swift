46808a9c83dcf8519da067c4d26eb659a8004057
KenFai <ngkenfai@gmail.com>
2020-11-10 16:11:33 +0800

    Initial Commit

---
+++ StylishCourses/StylishCoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StylishCoursesApp.swift
+//  StylishCourses
+//
+//  Created by Ginger on 10/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct StylishCoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
